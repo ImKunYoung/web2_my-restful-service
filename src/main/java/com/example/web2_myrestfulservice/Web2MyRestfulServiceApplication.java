a38a0f6b239162bb856dca0532ad16b262c166cb
ImKunYoung <lim8667@gmail.com>
2022-09-16 18:43:33 +0900

    first commit

---
+++ src/main/java/com/example/web2_myrestfulservice/Web2MyRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.web2_myrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2MyRestfulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2MyRestfulServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web2_myrestfulservice/Web2MyRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2_myrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2MyRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
