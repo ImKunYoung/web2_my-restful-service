cbc406e0d4b4aef32bc69559276ad447817ab644
ImKunYoung <lim8667@gmail.com>
2022-09-22 11:45:27 +0900

    calculateController 완성

---
+++ src/main/java/com/example/web2_myrestfulservice/CalculateController.java
@@ -0,0 +1,33 @@
+package com.example.web2_myrestfulservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/calculate/")
+@RestController
+public class CalculateController {
+
+    @GetMapping("/add/{a}/{b}")
+    public Object add(@PathVariable("a") int a, @PathVariable("b") int b, Model model) {
+        return ResponseEntity.ok(model.addAttribute("더하기 결과입니다: ", a+b));
+    }
+    @GetMapping("/minus/{a}/{b}")
+    public Object minus(@PathVariable("a") int a, @PathVariable("b") int b, Model model) {
+        return ResponseEntity.ok(model.addAttribute("빼기 결과입니다: ", a-b));
+    }
+    @GetMapping("/multiply/{a}/{b}")
+    public Object multiply(@PathVariable("a") int a, @PathVariable("b") int b, Model model) {
+        return ResponseEntity.ok(model.addAttribute("곱하기 결과입니다: ", a*b));
+    }
+    @GetMapping("/divide/{a}/{b}")
+    public Object divide(@PathVariable("a") float a, @PathVariable("b") float b, Model model) {
+        return ResponseEntity.ok(model.addAttribute("나누기 결과입니다: ", a/b));
+    }
+
+
+
+}
